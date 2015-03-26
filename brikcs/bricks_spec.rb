34a8e71ff1129c0cfaa36c8b2da74a250122826c
yfractal <mike@beansmile.com>
2015-03-26 08:37:14 +0800

    empty tests for classes

---
+++ brikcs/bricks_spec.rb
@@ -0,0 +1,16 @@
+require "rspec"
+
+describe "Board" do
+  it "init board"
+  it "should show the boad friendly"
+end
+
+describe "Brick" do
+  it "init"
+  it "should show the brick friendly"
+  it "should return all uniq bricks (optional)"
+end
+
+describe "FillInBoard" do
+  it "find a solution"
+end
