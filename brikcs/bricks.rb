65da2ed5eca089760b24cb3f964d76314070dd46
yfractal <mike@beansmile.com>
2015-03-26 08:31:00 +0800

    add brick info

---
+++ brikcs/bricks.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+# question describion:
+# https://ruby-china.org/topics/22166
+#
+# 拼图的单位为[积木块]
+# [积木条]由5个[积木块]组成，每个[积木块]面与面相贴(二维界面)，可组合成任何可以组成的[积木形状]
+# [积木条]放在[拼盘]中无间隙放满([积木条]形状类型不同重复)即成功,[拼盘]长宽为3x20(单位[积木块])
+
+# an old and uncomplete solution: https://gist.github.com/yfractal/9fd567789ec0ef1534d0
+
